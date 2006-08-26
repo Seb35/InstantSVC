794f0a4752ec5effa9dfd30c75aa8cc17349845f
gron <gron@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2006-08-26 13:07:38 +0000

    Changed the TestClient.java according to the generated WSDL
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/trunk@372 4c906e0b-d405-0410-8fec-a64e80c61ebf

--- testing/Java/sampleClient/src/TestClient.java
+++ testing/Java/sampleClient/src/TestClient.java
@@ -11,38 +11,34 @@ import org.apache.ws.security.WSConstants;
 import org.apache.ws.security.handler.WSHandlerConstants;
 import org.apache.ws.security.message.token.UsernameToken;
 
-import de.tele_task.model.Lecture;
-import de.tele_task.services.TeleTaskServiceLocator;
-import de.tele_task.services.TeleTaskServiceSoap;
+import org.example.queryLecture.*;
 
-/**
- * 
- */
-
-/**
- * @author GRon
- *
- */
 public class TestClient {
 	public static void main(String[] args) {
 		EngineConfiguration cfg = new FileProvider("axis.wsdd");
-		TeleTaskServiceLocator locator = new TeleTaskServiceLocator(cfg);
+		QueryLectureLocator locator = new QueryLectureLocator(cfg);
 		try {
-			URL url = new URL("http://localhost:2222/webp/repo/source/soap.php");
-			TeleTaskServiceSoap stub = locator.getTeleTaskServiceSoap(url);
+			URL url = new URL("http://localhost:8080/webp/repo/example/teletask/soap.php/queryLecture");
+			QueryLecturePortType stub = locator.getqueryLecturePort(url);
 			Stub axisPort = (Stub)stub;
 			axisPort._setProperty(UsernameToken.PASSWORD_TYPE, WSConstants.PW_DIGEST);
 			axisPort._setProperty(WSHandlerConstants.PW_CALLBACK_REF, new PWCallback());
-			axisPort._setProperty(WSHandlerConstants.USER, "Micha");
-			PWCallback.setLogin("Micha", "HPI");
+			axisPort._setProperty(WSHandlerConstants.USER, "Stefan");
+			PWCallback.setLogin("Stefan", "HPI");
 			try {
 				//Lecture[] lectures = stub.getAllLectures();
+				System.out.println("getLecture(106)");
 				Lecture l = stub.getLecture(106);
+				System.out.print("LectureId: ");
 				System.out.println(l.getId());
+				System.out.print("LectureName: ");
 				System.out.println(l.getName());
+				System.out.println();
+				System.out.println("getAllLectures()");
 				Lecture[] lectures = stub.getAllLectures();
 				//System.out.println(lectures);
 				for (Lecture lecture : lectures) {
+					System.out.print("LectureName: ");
 					System.out.println(lecture.getName());
 				}
 
