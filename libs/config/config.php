838256f2700618a8aff5427bf1ce850b7f605dff
gron <gron@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2006-09-19 08:40:11 +0000

    rework directory structure
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@418 4c906e0b-d405-0410-8fec-a64e80c61ebf

--- libs/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('SMARTY_DIR'))
-    define('SMARTY_DIR', realpath(dirname(__FILE__).'/../../../smarty/').'/');
-if (!defined('ADODB_DIR'))
-    define('ADODB_DIR', realpath(dirname(__FILE__).'/../../../adodb/').'/');
-if (!defined('ROOT_DIR'))
-    define('ROOT_DIR', realpath(dirname(__FILE__).'/../').'/');
-
-?>
