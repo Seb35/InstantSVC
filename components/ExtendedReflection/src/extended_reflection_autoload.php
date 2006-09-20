7200a7f94a2f9f37faba6b6fe93c68904e017044
gron <gron@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2006-09-20 08:51:48 +0000

    now the Extended Reflection API should be ready to be used as ezc style component
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@434 4c906e0b-d405-0410-8fec-a64e80c61ebf

---
+++ components/ExtendedReflection/src/extended_reflection_autoload.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Autoloader definition for the File component.
+ *
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package ExtendedReflection
+ */
+
+return array(
+    'ExtendedReflectionApi' => 'ExtendedReflection/extended_reflection.php',
+    'ExtReflectionClass' => 'ExtendedReflection/class.php',
+    'ExtReflectionMethod' => 'ExtendedReflection/method.php',
+    'ExtReflectionProperty' => 'ExtendedReflection/property.php',
+    'ExtReflectionParameter' => 'ExtendedReflection/parameter.php',
+    'ExtReflectionExtension' => 'ExtendedReflection/extension.php',
+    'ExtReflectionFunction' => 'ExtendedReflection/function.php',
+    'TypeFactory' => 'ExtendedReflection/type_factory.php',
+    'Type' => 'ExtendedReflection/interfaces/type.php',
+    'TypeFactoryImpl' => 'ExtendedReflection/interfaces/type_factory.php',
+    'TypeMapper' => 'ExtendedReflection/type_mapper.php',
+    'PHPDocReturnTag' => 'ExtendedReflection/tags/return.php',
+    'PHPDocParamTag' => 'ExtendedReflection/tags/param.php',
+    'PHPDocVarTag' => 'ExtendedReflection/tags/var.php',
+    'PHPDocRestInTag' => 'ExtendedReflection/tags/rest_in.php',
+    'PHPDocRestOutTag' => 'ExtendedReflection/tags/rest_out.php',
+    'PHPDocRestMethodTag' => 'ExtendedReflection/tags/rest_method.php',
+    'PHPDocWebMethodTag' => 'ExtendedReflection/tags/webmethod.php',
+    'PHPDocWebServiceTag' => 'ExtendedReflection/tags/webservice.php',
+    'PHPDocParser' => 'ExtendedReflection/phpdoc/parser.php',
+    'PHPDocTagFactory' => 'ExtendedReflection/phpdoc/tag_factory.php',
+    'PHPDocTag' => 'ExtendedReflection/phpdoc/tag.php',
+    'ClassType' => 'ExtendedReflection/types/class_type.php',
+    'AbstractType' => 'ExtendedReflection/types/abstract_type.php',
+    'ArrayType' => 'ExtendedReflection/types/array_type.php',
+    'PrimitiveType' => 'ExtendedReflection/types/primitive_type.php'
+);
+
+?>
