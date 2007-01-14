6ac6407ad6b79bed7fa4411842d8468dce0444bf
gg <gg@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2007-01-14 18:37:51 +0000

    added autoload for WsdlGenerator
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@502 4c906e0b-d405-0410-8fec-a64e80c61ebf

---
+++ components/Wsdl/src/wsdl_autoload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Wsdl
+ * @copyright Copyright (C) 2007 InstantSVC Team
+ * @author Gregor Gabrysiak <gregor_abrak at web dot de>
+ * @license http://www.apache.org/licenses/LICENSE-2.0   Apache License 2.0
+ * @version //autogentag//
+ * @filesource
+ */
+
+return array(
+	'isvcWsdlGenerator' => 'Wsdl/wsdl_generator.php',
+);
+?>
