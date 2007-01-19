734146fed59be658a86fa45d0e2e0c2c339d98dc
fakko <fakko@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2007-01-19 12:47:26 +0000

    added documentation for the new plugin interface
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@506 4c906e0b-d405-0410-8fec-a64e80c61ebf

--- components/Wsdl/src/interfaces/wsdl_generator_plugin.php
+++ components/Wsdl/src/interfaces/wsdl_generator_plugin.php
@@ -1,12 +1,30 @@
 <?php
 /**
+ * @package    Wsdl
+ * @author     Gregor Gabrysiak <gregor_abrak at web dot de>
+ * @author     Falko Menge <mail@falko-menge.de>
+ * @copyright  2007 InstantSVC Team
+ * @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+/**
  * This interface is used for plugins, which are able to sort out all methods
  * which are not supposed to be published. Also, the plugins should be able
  * enrich the methods with documentation.
  */
 interface isvcWsdlGeneratorPlugin
 {
-    public function getPublishedMethods($myMethods);
-    public function getUserComments($myMethods);   
+    /**
+     * filters methods to be published in WSDL
+     * @param ReflectionMethod[] $methods list of method objects
+     * @return ReflectionMethod[] filtered list of method objects
+     */
+    public function getPublishedMethods($methods);
+    
+    /**
+     * provides an array with documentation for each method
+     * @param ReflectionMethod[] $methods - Liste mit ReflectionMethod-Objekten
+     * @return array(string=>string) documentation texts with method name as key
+     */
+    public function getUserComments($methods);
 }
 ?>
