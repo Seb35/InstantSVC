ed694404b62f2c50516d6fe9aa9b928d994c08a1
gron <gron@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2007-08-24 16:27:33 +0000

    add autoload for ezc scripts/create_pear_package.php
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@911 4c906e0b-d405-0410-8fec-a64e80c61ebf

---
+++ components/autoload/base_autoload.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package Base
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ */
+
+return array(
+    'ezcBaseConfigurationInitializer'      => 'Base/interfaces/configuration_initializer.php',
+    'ezcBaseFeatures'                      => 'Base/features.php',
+    'ezcBaseInit'                          => 'Base/init.php',
+    'ezcBaseOptions'                       => 'Base/options.php',
+    'ezcBaseStruct'                        => 'Base/struct.php',
+
+    'ezcBaseRepositoryDirectory'           => 'Base/structs/repository_directory.php',
+
+    'ezcBaseException'                     => 'Base/exceptions/exception.php',
+    'ezcBaseFileException'                 => 'Base/exceptions/file_exception.php',
+
+    'ezcBaseAutoloadException'             => 'Base/exceptions/autoload.php',
+    'ezcBaseDoubleClassRepositoryPrefixException' => 'Base/exceptions/double_class_repository_prefix.php',
+    'ezcBaseExtensionNotFoundException'    => 'Base/exceptions/extension_not_found.php',
+    'ezcBaseFileNotFoundException'         => 'Base/exceptions/file_not_found.php',
+    'ezcBaseFileIoException'               => 'Base/exceptions/file_io.php',
+    'ezcBaseFilePermissionException'       => 'Base/exceptions/file_permission.php',
+    'ezcBaseInitCallbackConfiguredException' => 'Base/exceptions/init_callback_configured.php',
+    'ezcBaseInvalidParentClassException'   => 'Base/exceptions/invalid_parent_class.php',
+    'ezcBasePropertyPermissionException'   => 'Base/exceptions/property_permission.php',
+    'ezcBasePropertyNotFoundException'     => 'Base/exceptions/property_not_found.php',
+    'ezcBaseSettingNotFoundException'      => 'Base/exceptions/setting_not_found.php',
+    'ezcBaseSettingValueException'         => 'Base/exceptions/setting_value.php',
+    'ezcBaseValueException'                => 'Base/exceptions/value.php',
+);
+?>
