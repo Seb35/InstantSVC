7b817021350c61f024874a7eb7ed1e5cc10384e5
gron <gron@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2007-08-25 09:37:08 +0000

    fixed problem with adapter access
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/branches/transition@918 4c906e0b-d405-0410-8fec-a64e80c61ebf

--- components/Wsdl/tests/dev-suite.php
+++ components/Wsdl/tests/dev-suite.php
@@ -7,6 +7,7 @@ function __autoload( $className ) { ezcBase::autoload( $className ); }
 
 require_once dirname(__FILE__).'/../src/interfaces/wsdl_generator_plugin.php';
 require_once dirname(__FILE__).'/../src/wsdl_generator.php';
+require_once dirname(__FILE__).'/../../Soap/src/adapter_generator.php';
 
 include('suite.php');
 ?>
