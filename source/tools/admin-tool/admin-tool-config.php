f8aa37c91705cf982b6d2c55ddd1891178efd6be
fakko <fakko@4c906e0b-d405-0410-8fec-a64e80c61ebf>
2008-01-06 22:05:31 +0000

    - Platform neutral PHP_BIN_PATH
    git-svn-id: svn://svn.code.sf.net/p/instantsvc/code/trunk@1084 4c906e0b-d405-0410-8fec-a64e80c61ebf

--- source/tools/admin-tool/admin-tool-config.php
+++ source/tools/admin-tool/admin-tool-config.php
@@ -4,5 +4,5 @@ define ('AT_ROOT', dirname(__FILE__));
 define ('ADODB_DIR', dirname(__FILE__).'/../../../../adodb/');
 define ('SMARTY_DIR', dirname(__FILE__).'/../../../../smarty/');
 define ('AT_SMARTY_TEMP_DIR', dirname(__FILE__).'/../../');
-define('PHP_BIN_PATH', 'C:\Programme\PHP\php.exe -c C:\Programme\PHP');
+define('PHP_BIN_PATH', 'php');
 ?>
